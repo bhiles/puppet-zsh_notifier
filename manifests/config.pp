5de9effbd07862b6c48a02fdb4061d15034db4b3
bhiles <bennetthiles@gmail.com>
2014-05-14 10:08:48 -0700

    correct errors found when running tests

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,12 +1,19 @@
 # Setup the env variable template file
 class zsh_notifier::config {
-  # include boxen::config
+
+  $notify_timeout               = 1
+  $terminal_notifier_executable = '/tmp/'
+  $zsh_notify_executable        = '/tmp/'
+
+  include boxen::config
+
   boxen::env_script { 'zsh_notifier':
     content  => template('zsh_notifier/env.sh.erb'),
+    require => Repository['zsh_notify'],
   }
 
   file { "${boxen::config::envdir}/zsh_notifier.sh":
-    ensure => absent,
+    ensure  => absent,
   }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,11 @@
-# This is a placeholder class.
+# Main class to install dependencies
 class zsh_notifier {
-  anchor { 'Hello_World': }
 
   package { 'terminal_notifier':
-    ensure   => present,
+    #ensure   => present,
     provider => 'compressed_app',
     source   => 'https://github.com/alloy/terminal-notifier/releases/tag/1.5.0',
   }
-
-  #package { 'zsh_notify':
-  #  ensure   => present,
-  #  provider => 'compressed_app',
-  #  source   => 'http://unknown',
-  #  require  => Package['terminal-notifier'],
-  #}
 
   repository { 'zsh-nofity-repo':
     provider => 'git',
@@ -22,21 +14,4 @@ class zsh_notifier {
     require  => Package['terminal_notifier'],
   }
 
-  class { 'zsh_notifier::config':
-    ensure                       => present,
-    notify_timeout               => 1,
-    terminal_notifier_executable => '/tmp/',
-    zsh_notify_executable        => '/tmp/',
-    require                      => Repository['zsh_notify'],
-  }
-
-  #  command => "echo '[ -f /opt/boxen/test.sh ] && source /opt/boxen/test.sh' >> ~/.profile",
-  #  require => Package['zsh_notify'],
-  #}
-
-# install terminal_notifier
-# return access to it's executable
-#class terminal_notifier {
-#  provider:
-#}
 }
