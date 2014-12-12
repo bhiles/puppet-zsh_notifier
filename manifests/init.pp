24ae529e2d0ee56d015b670caac990e34b869ed3
Bennett Hiles <bennetthiles@gmail.com>
2014-12-11 16:02:02 -0800

    Update package name to terminal-notifier-1.5.0

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 # Main class to install dependencies
 class zsh_notifier {
 
-  package { 'terminal-notifier':
+  package { 'terminal-notifier-1.5.0':
     ensure   => present,
     provider => 'compressed_app',
     source   => 'https://github.com/alloy/terminal-notifier/releases/download/1.5.0/terminal-notifier-1.5.0.zip',
@@ -11,7 +11,7 @@ class zsh_notifier {
     provider => 'git',
     source   => 'marzocchi/zsh-notify',
     path     => '/opt/boxen/zsh_notify',
-    require  => Package['terminal-notifier'],
+    require  => Package['terminal-notifier-1.5.0'],
   }
 
   class {'zsh_notifier::config':
