f31ea92497da641d68334b09081001b971473635
Bennett Hiles <bennetthiles@gmail.com>
2014-12-12 10:10:26 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,17 @@
 # Main class to install dependencies
 class zsh_notifier {
 
-  package { 'terminal-notifier-1.5.0':
+  package { 'terminal-notifier':
     ensure   => present,
     provider => 'compressed_app',
-    source   => 'https://github.com/alloy/terminal-notifier/releases/download/1.5.0/terminal-notifier-1.5.0.zip',
+    source   => 'https://github.com/bhiles/puppet-zsh_notifier/raw/master/terminal-notifier.tar.gz',
   }
 
   repository { 'zsh_notify_repo':
     provider => 'git',
     source   => 'marzocchi/zsh-notify',
     path     => '/opt/boxen/zsh_notify',
-    require  => Package['terminal-notifier-1.5.0'],
+    require  => Package['terminal-notifier'],
   }
 
   class {'zsh_notifier::config':
