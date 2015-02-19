b493b7d3d0f3bc675d7316834befc7b14cb21fb1
Bennett Hiles <bennetthiles@gmail.com>
2015-02-18 21:41:18 -0500

    correct executable location

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,11 +3,10 @@ class zsh_notifier::config {
 
   include boxen::config
 
-  $notify_timeout        = 1
-  $terminal_notifier_dir = "/Applications/terminal-notifier-1.5.0"
-  $terminal_notifier_executable = "${terminal_notifier_dir}/terminal-notifier.app/Contents/MacOS/terminal-notifier"
-  $zsh_notify_dir        = "${boxen::config::home}/zsh_notify"
-  $zsh_notify_executable = "${zsh_notify_dir}/notify.plugin.zsh"
+  $notify_timeout               = 1
+  $terminal_notifier_executable = "/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier"
+  $zsh_notify_dir               = "${boxen::config::home}/zsh_notify"
+  $zsh_notify_executable        = "${zsh_notify_dir}/notify.plugin.zsh"
 
   boxen::env_script { 'zsh_notifier':
     content  => template('zsh_notifier/env.sh.erb'),
